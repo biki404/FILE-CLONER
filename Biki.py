fbcc24743cc03beca5afad973f9fa1f16cb0eb9f
Biki Mahamood <92981044+biki404@users.noreply.github.com>
2022-03-22 13:58:50 +0530

    Biki.py

--- Biki.py
+++ Biki.py
@@ -1,7 +1,7 @@
-import requests
+import os, platform
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from biki import haha
-    haha()
+    from biki import reg
+    reg()
 elif bit == '32bit':
     print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
