37b45cd1b75526f348405e67c0fa59526f001f5f
Biki Mahamood <92981044+biki404@users.noreply.github.com>
2022-03-22 13:43:18 +0530

    File.py

--- FILE.py
+++ FILE.py
@@ -1,7 +1,7 @@
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from biki import reg
+    from biki1 import reg
     reg()
 elif bit == '32bit':
     print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
