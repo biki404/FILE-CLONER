d9678045b435ab6aa55a86d73d221bd9192df447
Biki Mahamood <92981044+biki404@users.noreply.github.com>
2022-03-22 13:35:35 +0530

    FILE.py

--- FILE.py
+++ FILE.py
@@ -1,18 +1,7 @@
-import os, platform,time
-try:
-   import requests
-except:
-   os.system('pip2 install requests')
-from time import sleep
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from biki import xsxsxlll
-    print("\n Congratulations! Your device supported!\n")
-    time.sleep(3)
-    xsxsxlll()
+    from biki import reg
+    reg()
 elif bit == '32bit':
-    from biki import xsxsxlll
-    print("\n Congratulations! Your device supported!\n")
-    time.sleep(3)
-    xsxsxlll()
+    print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
